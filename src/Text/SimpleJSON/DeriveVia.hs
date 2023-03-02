3c7cec01935fc7747739b2f656136bd396de0c34
SeeMemes <provotorov-02@mail.ru>
2023-03-02 17:46:36 +0300

    -signatures

--- src/Text/SimpleJSON/DeriveVia.hs
+++ src/Text/SimpleJSON/DeriveVia.hs
@@ -9,7 +9,5 @@ import Text.SimpleJSON.Types ( JSValue )
 newtype CustomJSON a = CustomJSON { unCustomJSON :: a }
 
 instance (Data a) => JSON (CustomJSON a) where
-  readJSON :: Data a => JSValue -> Result (CustomJSON a)
   readJSON = (coerce `asTypeOf` fmap CustomJSON) . fromJSONGeneric
-  showJSON :: Data a => CustomJSON a -> JSValue
   showJSON = toJSONGeneric . unCustomJSON
